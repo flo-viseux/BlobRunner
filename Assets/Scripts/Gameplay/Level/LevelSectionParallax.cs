3e719c4cb110473cc31b1519018b6d68e3e2b79d
flo-viseux <frinola.games@gmail.com>
2024-04-17 20:43:27 +0200

    changement atelier + changement version

--- Assets/Scripts/Gameplay/Level/LevelSectionParallax.cs
+++ Assets/Scripts/Gameplay/Level/LevelSectionParallax.cs
@@ -3,44 +3,6 @@ using UnityEngine;
 
 public class LevelSectionParallax : Parallax
 {
-    /*#region SerializedFields
-    [SerializeField] private LevelSection[] sections;
-
-    [SerializeField] private float speed = 1.0f;
-    #endregion
-
-    #region Attributes
-    private float currentDist;
-
-    private float sectionDist;
-    #endregion
-
-    #region UnityMethods
-    private void Awake()
-    {
-        InstantiateRandomSection();
-    }
-
-    private void Update()
-    {
-        currentDist += Time.deltaTime * speed;
-
-        if (currentDist >= sectionDist) 
-        {
-            InstantiateRandomSection();
-        }
-    }
-    #endregion
-
-    #region Private
-    private void InstantiateRandomSection()
-    {
-        LevelSection newSection = Instantiate(sections[Random.Range(0, sections.Length)], this.transform);
-        newSection.speed = speed;
-        sectionDist += newSection.Size;
-    }
-    #endregion*/
-
     #region Serialized fields
     [SerializeField] private float offset = -2f;
 
