2cfdc1fa1187af0e896b07f0a5849e2396bc641c
Laurene <laurene.broquet@gmail.com>
2024-07-07 17:32:57 +0200

    Scriptable objects for events

---
+++ Assets/Scripts/Events/SFX_EventSO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+
+    [CreateAssetMenu(fileName = "SFX_Event", menuName = "Events/New SFX Event", order = 0)]
+    public class SFX_EventSO : ScriptableObject
+    {
+        public event Action<AudioManager.EType> OnJumpEffectRaise;
+        public event Action<AudioManager.ETypeDive> OnDiveEffectRaise;
+
+        public void RaiseJumpEvent(AudioManager.EType sfxJumpType)
+        {
+            OnJumpEffectRaise?.Invoke(sfxJumpType);
+        }
+        
+        public void RaiseDiveEvent(AudioManager.ETypeDive sfxDiveType)
+        {
+            OnDiveEffectRaise?.Invoke(sfxDiveType);
+        }
+    }
