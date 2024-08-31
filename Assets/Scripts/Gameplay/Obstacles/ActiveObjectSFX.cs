83af45e8fba13efa27295e9db29db6211dc2613b
Laurene <laurene.broquet@gmail.com>
2024-08-31 11:10:29 +0200

    add sfx to obstacles

---
+++ Assets/Scripts/Gameplay/Obstacles/ActiveObjectSFX.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.Obstacles
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class ActiveObjectSFX : MonoBehaviour
+    {
+        private AudioSource _source;
+        [SerializeField] private AudioSource sourceForStart;
+        [SerializeField] private bool isTruck = false;
+
+        private void Awake()
+        {
+            _source = GetComponent<AudioSource>();
+        }
+
+        private void OnEnable()
+        {
+            if (isTruck && sourceForStart != null)
+            {
+                sourceForStart.PlayOneShot(sourceForStart.clip);
+            }
+            _source.Play();
+        }
+
+        public void OnDisable()
+        {
+            _source.Stop();
+        }
+    }
+}
